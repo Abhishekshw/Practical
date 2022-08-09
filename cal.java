91416a9b35a59199e3a8512db2a22505cb84d5e0
Abhishekshw <102210901+Abhishekshw@users.noreply.github.com>
2022-08-09 17:47:24 +0530

    Add files via upload

---
+++ cal.java
@@ -0,0 +1,16 @@
+class cal{
+public static void main(String []args){
+System.out.println("enter 2 no");
+int sum,mul,div,sub;
+int a=12;
+int b=6;
+System.out.println(a+ " " +b );
+sum=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+System.out.println("the sum is=" + sum);
+System.out.println("the subtraction is =" + sub);
+System.out.println("the multiplication is =" + mul);
+System.out.println("the division is =" + div);
+}}
