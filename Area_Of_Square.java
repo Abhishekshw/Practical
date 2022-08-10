68bec09cd1757d810475e8673c03c5bd3a18bf26
Abhishekshw <102210901+Abhishekshw@users.noreply.github.com>
2022-08-10 15:37:12 +0530

    Add files via upload

---
+++ Area_Of_Square.java
@@ -0,0 +1,11 @@
+class Area_Of_Square{
+public static void main(String[]args){
+float length,perimeter,area;
+System.out.println("enter side of square");
+length=4;
+System.out.println("side of square is " +length);
+perimeter=4*length;
+area=length*length;
+System.out.println("your perimeter is="+ perimeter);
+System.out.println("enter side of square"+area);
+}}
