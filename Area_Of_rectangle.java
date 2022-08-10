28ef8b0f89ed4c89572dac923ef65c7f502ecd14
Abhishekshw <102210901+Abhishekshw@users.noreply.github.com>
2022-08-10 15:36:56 +0530

    Add files via upload

---
+++ Area_Of_rectangle.java
@@ -0,0 +1,12 @@
+class Area_Of_rectangle{
+public static void main(String[]args){
+float length,breadth,perimeter,area;
+System.out.println("enter side of square");
+length=4;
+breadth=6;
+System.out.println("side of square is " +length+ " the breadth is " + breadth);
+perimeter=2*(length+breadth);
+area=breadth*length;
+System.out.println("your perimeter is="+ perimeter);
+System.out.println("enter side of square"+area);
+}}
