60d28a4308e1d71a2a91a151aa2585500445e6a0
Abhishekshw <102210901+Abhishekshw@users.noreply.github.com>
2022-08-23 18:49:25 +0530

    Add files via upload

---
+++ Foreachloop.java
@@ -0,0 +1,17 @@
+package loop;
+
+public class Foreachloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0; //value of sum
+int arr[]= {1,2,3,4,5};//arrray value
+for(int i :arr) {
+	System.out.println(i);//o/p
+	sum=sum+i;
+}
+	System.out.println(sum);//result
+
+	}
+
+}
---
+++ Infinityfor.java
@@ -0,0 +1,9 @@
+package loop;
+
+public class Infinityfor {
+	public static void main(String[] args) {
+		for(;;) {System.out.println("need more practise");//starting infinty loop
+	}
+
+}
+}
