4fbe3410a9a9ee16b5a7b0aa795516e4363a1306
Abhishekshw <102210901+Abhishekshw@users.noreply.github.com>
2022-09-21 18:02:52 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,9 @@
+package encapculation;
+import encapculation.Student;
+public class App {
+public static void main(String[] args) {
+	Student t=new Student();
+	t.setName( "|Abhi");
+	System.out.println(t.getName());
+}
+}
--- App2.java
+++ App2.java
@@ -1,29 +1,17 @@
-package Interface;
-interface interface1{//creating an interface
-	void show();//by defdault allmethod in interface is public
-	void display();
-	void print();
-}
-//creating abstract class thet implement one interface
-abstract class Absclass implements interface1{
-	public void show() {
-		System.out.println("inside show method");
-	}
-}
-//creating sub class of abstract class
-class Class2 extends Absclass{
-	public void display() {
-		System.out.println("inside display method");
-	}
-	public void print()
-{
-		System.out.println("inside show method");}
-}
+
+	package encapculation;
+
+
 public class App2 {
-public static void main(String[] args) {
-	interface1 obj=new Class2();
-	obj.show();
-	obj.print();
-	obj.display();
+private String name="Abhi";
+public String getName() {
+	return name;
 }
+}
+class Main{
+	public static void main(String[] args) {
+		App2 A=new App2();
+		//A.setName("ABHI");
+		System.out.println(A.getName());
+	}
 }
